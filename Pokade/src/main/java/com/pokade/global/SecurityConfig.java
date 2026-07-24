7b9d744dad6f503409931f67ce67a9a9e54845a2
개발자1 <cm41823@gmail.com>
2026-07-24 15:01:33 +0900

    스웨거 경로 추가

--- Pokade/src/main/java/com/pokade/global/SecurityConfig.java
+++ Pokade/src/main/java/com/pokade/global/SecurityConfig.java
@@ -14,6 +14,11 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 );
         return http.build();
