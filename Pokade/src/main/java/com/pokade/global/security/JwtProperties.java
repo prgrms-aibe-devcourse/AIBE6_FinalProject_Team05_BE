525c32144ebf74f9975bf04a56a12804ca9fa906
predevho <dlagusgh7894@gmail.com>
2026-07-28 15:11:06 +0900

    feat: JwtProperties 추가 및 ConfigurationPropertiesScan 활성화

---
+++ Pokade/src/main/java/com/pokade/global/security/JwtProperties.java
@@ -0,0 +1,12 @@
+package com.pokade.global.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.time.Duration;
+
+@ConfigurationProperties(prefix = "jwt")
+public record JwtProperties(
+        String secret,
+        Duration accessExpiration
+) {
+}
