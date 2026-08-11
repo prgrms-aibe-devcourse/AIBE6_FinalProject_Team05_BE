5cf9ab37a7b79a43bb89d6e31fa702d1ba8b3ca5
predevho <dlagusgh7894@gamil.com>
2026-08-11 11:28:35 +0900

    feat: 소셜 탈퇴 재인증용 DTO 필드·에러코드 추가

--- Pokade/src/main/java/com/pokade/domain/user/dto/request/WithdrawalRequest.java
+++ Pokade/src/main/java/com/pokade/domain/user/dto/request/WithdrawalRequest.java
@@ -1,6 +1,7 @@
 package com.pokade.domain.user.dto.request;
 
 public record WithdrawalRequest(
-        String password
+        String password, // LOCAL 계정용 (소셜은 null)
+        String reauthToken // 소셜 계정용 OAuth 재인증 proof 티켓 (LOCAL은 null)
 ) {
 }
