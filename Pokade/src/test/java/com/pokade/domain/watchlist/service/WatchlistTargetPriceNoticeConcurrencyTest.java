56c49ccf21e51285cf49895df509dce7efd11f0c
ktk3376 <ktk3376@gmail.com>
2026-08-19 11:37:56 +0900

    fix: 트랜잭션 커밋 후 SSE를 전송

--- Pokade/src/test/java/com/pokade/domain/watchlist/service/WatchlistTargetPriceNoticeConcurrencyTest.java
+++ Pokade/src/test/java/com/pokade/domain/watchlist/service/WatchlistTargetPriceNoticeConcurrencyTest.java
@@ -104,7 +104,7 @@ class WatchlistTargetPriceNoticeConcurrencyTest {
         given(priceTradeStatsRepository.findPriceRangesByCardIds(any(), any(), any())).willReturn(List.of(range));
         given(priceTradeStatsRepository.findPriceRangesByCardIdsSince(any(), any(), any(), any())).willReturn(List.of(range));
 
-        NotificationService notificationService = new NotificationService(notificationRepository, new SseEmitterStore());
+        NotificationService notificationService = new NotificationService(notificationRepository, new SseEmitterStore(), event -> { });
         WatchlistService watchlistService = new WatchlistService(watchlistRepository,
                 mock(PriceService.class), cardRepository, priceTradeStatsRepository, mock(CardNameKoResolver.class),
                 notificationService);
